c6fe6b35a8a97c99d49862e75a5d6ee58e2cdafc
Allen Hujsak <allen@missionvi.com>
2019-08-13 08:09:26 -0400

    hooked up navigation to routes

--- src/scenes/Navigation.js
+++ src/scenes/Navigation.js
@@ -1,15 +1,37 @@
 // @flow
-
+import {
+  ADDRESS_ROUTE,
+  BANK_CONNECT_ROUTE,
+  DOC_UPLOAD_ROUTE,
+  NEW_USER_NAME_ROUTE,
+  PHONE_ROUTE,
+  SIGNATURE_ROUTE,
+  SOCIAL_ROUTE,
+  START_ROUTE,
+  THANK_YOU_ROUTE,
+  TRANSACTION_AMOUNT_ROUTE,
+  TRANSACTION_CONFIRM_ROUTE,
+  TRANSACTION_SUCCESS_ROUTE
+} from '../constants/index'
+import {
+  AddressConnector,
+  BuySellConnector,
+  DocUploadConnector,
+  NewUserConnector,
+  PhoneConnector,
+  SignatureConnector,
+  SocialConnector,
+  StartSceneConnector,
+  ThankYouConnector,
+  TransactionAmountConnector,
+  TransactionConfirmationConnector,
+  TransactionSuccessConnector
+} from '../connectors/indexConnectors'
 import React, { Component } from 'react'
 import { Route, HashRouter as Router } from 'react-router-dom'
 import { createMuiTheme, withStyles } from 'material-ui/styles'
 
-// import { ConfirmationScreenConnector as Confirmation } from '../connectors/ConfirmationScreenConnector'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import { NewUserNameScene } from './NewUserNameScene'
-// import { SellQuoteRequestSceneConnector as SellQuoteRequestScene } from '../connectors/SellQuoteRequestSceneConnector'
-// import { StartSceneConnector as StartScene } from '../connectors/StartSceneConnector'
-// import StartScene from './InputSceneTest'
 import history from '../history/history';
 
 const theme = createMuiTheme({
@@ -28,18 +50,54 @@ const theme = createMuiTheme({
 })
 
 export const routes = [{
-  path: '/',
-  main: NewUserNameScene,
-  exact: true
-}/* , {
-  path: '/sellQuoteRequest',
-  main: SellQuoteRequestScene,
+  path: START_ROUTE,
+  main: StartSceneConnector,
   exact: true
 }, {
-  path: '/ConfirmationScene',
-  main: Confirmation,
+  path: ADDRESS_ROUTE,
+  main: AddressConnector,
   exact: true
-} */]
+}, {
+  path: BANK_CONNECT_ROUTE,
+  main: BuySellConnector,
+  exact: true
+}, {
+  path: DOC_UPLOAD_ROUTE,
+  main: DocUploadConnector,
+  exact: true
+}, {
+  path: NEW_USER_NAME_ROUTE,
+  main: NewUserConnector,
+  exact: true
+}, {
+  path: PHONE_ROUTE,
+  main: PhoneConnector,
+  exact: true
+}, {
+  path: SIGNATURE_ROUTE,
+  main: SignatureConnector,
+  exact: true
+}, {
+  path: SOCIAL_ROUTE,
+  main: SocialConnector,
+  exact: true
+}, {
+  path: THANK_YOU_ROUTE,
+  main: ThankYouConnector,
+  exact: true
+}, {
+  path: TRANSACTION_AMOUNT_ROUTE,
+  main: TransactionAmountConnector,
+  exact: true
+}, {
+  path: TRANSACTION_CONFIRM_ROUTE,
+  main: TransactionConfirmationConnector,
+  exact: true
+}, {
+  path: TRANSACTION_SUCCESS_ROUTE,
+  main: TransactionSuccessConnector,
+  exact: true
+}]
 
 const appStyles = (theme) => ({
   content: {
