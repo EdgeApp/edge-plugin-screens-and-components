494bf3e0ca1904a863ae136f8e9fe02dbf12cfe9
Allen Hujsak <allen@missionvi.com>
2019-08-15 12:41:00 -0400

    fix import export

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { TransactionAmountScreen } from './scenes/TransactionAmountScreen'
 
 const root = document.getElementById('root')
 // $FlowFixMe
 ReactDOM.render(<App />, root)
 
-export { TransactionAmountScreen } from './scenes/TransactionAmountScreen'
+export {
+  TransactionAmountScreen
+}
 
 
